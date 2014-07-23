6bdd68af6cfeae3466f33beaae00125d239f684b
Ryan Hauert <ryanoffski@gmail.com>
2014-07-23 08:22:46 -0600

    Added test project

---
+++ src/Immutability.Tests/SlowImmutableListTests.cs
@@ -0,0 +1,39 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Immutability.Tests
+{
+    public class SlowImmutableListTests
+    {
+        [Fact]
+        public void AddDoesNotAffectOriginalList()
+        {
+            var list = new SlowImmutableList<int>();
+            var list2 = list.Add(1);
+
+            list2.ShouldContain(1);
+            list.ShouldNotContain(1);
+        }
+
+        [Fact]
+        public void RemoveDoesNotAffectOriginalList()
+        {
+            var list = new SlowImmutableList<int>(new[] { 1, 2, 3, 4 });
+            var list2 = list.Remove(1);
+
+            list2.ShouldNotContain(1);
+            list.ShouldContain(1);
+        }
+
+        [Fact]
+        public void CanEnumerateListWhileSomethingChangesIt()
+        {
+            var list = new SlowImmutableList<int>(new[] { 1, 2, 3, 4 });
+            var enumerator = list.GetEnumerator();
+            enumerator.MoveNext().ShouldBe(true);
+
+            var list2 = list.Add(1);
+            Should.NotThrow(() => enumerator.MoveNext().ShouldBe(true));
+        }
+    }
+}
