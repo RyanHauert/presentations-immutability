6ef525ee127cac3300628d6bea1621cfe4411167
Ryan Hauert <ryanoffski@gmail.com>
2014-07-23 07:59:40 -0600

    Added SlowImmutableList example

---
+++ src/Immutability/SlowImmutableList.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Immutability
+{
+    public class SlowImmutableList<T> : IEnumerable<T>
+    {
+        private readonly IList<T> _inner = new List<T>();
+
+        public SlowImmutableList()
+        {
+        }
+
+        public SlowImmutableList(IList<T> list)
+        {
+            _inner = list;
+        }
+
+        public SlowImmutableList<T> Add(T item)
+        {
+            var copy = _inner.ToList();
+            copy.Add(item);
+            return new SlowImmutableList<T>(copy);
+        }
+
+        public SlowImmutableList<T> Remove(T item)
+        {
+            var copy = _inner.ToList();
+            copy.Remove(item);
+            return new SlowImmutableList<T>(copy);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _inner.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
