9ff637b0ee0d59c14ef678b10f04b47f19006ba2
Ryan Hauert <ryanoffski@gmail.com>
2014-08-01 11:29:46 -0600

    Added Cache example

---
+++ src/Immutability.Tests/Examples/CacheTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using Immutability.Examples;
+using Shouldly;
+using Xunit;
+
+namespace Immutability.Tests.Examples
+{
+    public class CacheTests
+    {
+        [Fact]
+        public void CanAdd()
+        {
+            var cache = new Cache<string, string>();
+            cache["Foo"] = "Bar";
+
+            cache.ContainsKey("Foo").ShouldBe(true);
+            cache["Foo"].ShouldBe("Bar");
+        }
+
+        [Fact]
+        public void CanGetFromCacheWhenMissing()
+        {
+            var cache = new Cache<string, string>(onMissing: x => x);
+            cache["Foo"].ShouldBe("Foo");
+            cache["Bar"].ShouldBe("Bar");
+        }
+
+        [Fact]
+        public void ThrowsIfNotProvidedWithFunc()
+        {
+            var cache = new Cache<string, string>();
+            Should.Throw<KeyNotFoundException>(() => { var _ = cache["Foo"]; });
+        }
+
+        [Fact]
+        public void CanClear()
+        {
+            var existing = new Dictionary<string, string>();
+            existing["Foo"] = "Bar";
+
+            var cache = new Cache<string, string>(existing);
+            cache.Count.ShouldBe(1);
+            cache.Clear();
+            cache.Count.ShouldBe(0);
+        }
+
+        [Fact]
+        public void CanRemove()
+        {
+            var cache = new Cache<string, string>();
+            cache["Foo"] = "Bar";
+
+            cache.Remove("Foo").ShouldBe(true);
+            cache.ContainsKey("Foo").ShouldBe(false);
+        }
+    }
+}
---
+++ src/Immutability/Examples/Cache.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using FubuCore;
+
+namespace Immutability.Examples
+{
+    public class Cache<TKey, TValue> : IEnumerable<TValue>
+    {
+        private readonly Func<TKey, TValue> _onMissing =
+            x => { throw new KeyNotFoundException("Key '{0}' could not be found".ToFormat(x)); };
+
+        private ImmutableDictionary<TKey, TValue> _values;
+
+        public Cache(IDictionary<TKey, TValue> values = null, Func<TKey, TValue> onMissing = null)
+        {
+            _values = values == null
+                ? ImmutableDictionary<TKey, TValue>.Empty
+                : ImmutableDictionary.CreateRange(values);
+
+            if (onMissing != null)
+            {
+                _onMissing = onMissing;
+            }
+        }
+
+        public int Count
+        {
+            get { return _values.Count; }
+        }
+
+        public TValue this[TKey key]
+        {
+            get { return ImmutableInterlocked.GetOrAdd(ref _values, key, _onMissing); }
+            set { ImmutableInterlocked.AddOrUpdate(ref _values, key, value, (x, old) => value); }
+        }
+
+        public void Clear()
+        {
+            _values = _values.Clear();
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            return _values.ContainsKey(key);
+        }
+
+        public bool Remove(TKey key)
+        {
+            TValue value;
+            return ImmutableInterlocked.TryRemove(ref _values, key, out value);
+        }
+
+        public IEnumerator<TValue> GetEnumerator()
+        {
+            return _values.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
